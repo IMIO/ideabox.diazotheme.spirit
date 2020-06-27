b7cea41be61c872f6f4f7e2f0f567abf045decb1
Martin Peeters <martin.peeters@affinitic.be>
2020-06-27 16:37:38 +0200

    Compile CSS and add an upgrade step to reload resources

--- src/ideabox/diazotheme/spirit/upgrades.py
+++ src/ideabox/diazotheme/spirit/upgrades.py
@@ -3,6 +3,13 @@
 from plone import api
 
 
+def reload_registry(context):
+    portal_setup = api.portal.get_tool("portal_setup")
+    portal_setup.runImportStepFromProfile(
+        "profile-ideabox.diazotheme.spirit:default", "plone.app.registry"
+    )
+
+
 def to_1001(context):
     """ Add a portal action for informations """
     portal_setup = api.portal.get_tool("portal_setup")
