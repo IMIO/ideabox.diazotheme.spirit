6a8284a8d33e0592d810fbcdb8618862813cb856
Martin Peeters <martin.peeters@affinitic.be>
2020-06-29 10:40:08 +0200

    Back to development: 1.2.0a14

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='ideabox.diazotheme.spirit',
-    version='1.2.0a13',
+    version='1.2.0a14.dev0',
     description="Spirit theme for Ideabox",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
